0b56fa692ed692de4de1a4fa37d1f7d194210e1f
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-08 10:04:04 -0500

    added an inline declaration and assignment of values for an array

--- CSharpFundamentalsUnderstandingArrays/Program.cs
+++ CSharpFundamentalsUnderstandingArrays/Program.cs
@@ -6,12 +6,16 @@ namespace CSharpFundamentalsUnderstandingArrays
     {
         static void Main(string[] args)
         {
+            /*
             int[] numbers = new int[5];
             numbers[0] = 4;
             numbers[1] = 8;
             numbers[2] = 15;
             numbers[3] = 16;
             numbers[4] = 23;
+            */
+
+            int[] numbers = new int[] { 4, 8, 15, 16, 23, 41 };
 
             Console.WriteLine(numbers[1].ToString());
             Console.ReadLine();
