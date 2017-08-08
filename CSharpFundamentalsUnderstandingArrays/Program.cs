1e26e563fcd6535515b5960732ee7580ccb812ea
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-08 09:54:24 -0500

    Add project files.

---
+++ CSharpFundamentalsUnderstandingArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFundamentalsUnderstandingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
